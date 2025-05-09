27a9ea0a105da0602c73ac9c38536797cd7b9474
Daydream127 <131152155+Daydream127@users.noreply.github.com>
2025-05-09 21:27:24 +0100

    fazer umas ligacoes e arranajr o footer e perfis vazios

---
+++ utad.PlayAble/Pages/Home/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace utad.PlayAble.Pages.Home
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
